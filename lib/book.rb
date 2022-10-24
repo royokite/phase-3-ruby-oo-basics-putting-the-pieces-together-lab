f8f0e99e00c7f00622830d73483ad9282c68bedf
RoyOkite <royokite@gmail.com>
2022-10-24 13:16:08 +0300

    Completed assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,26 @@
+class Book
+    attr_accessor :title, :author, :page_count, :genre
 
+    def initialize title
+        @title = title
+    end
 
+    def properties title = @title, author, page_count, genre
+        @title = title
+        @author = author
+        @page_count = page_count
+        @genre = genre
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+
+book = Book.new("Legendary Programmer")
+book.properties("Rokyt", "69", "ACtion",)
+puts book.title
+puts book.author
+puts book.page_count
+puts book.genre
+book.turn_page
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,31 @@
-# Make your shoe class here!
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+
+    def initialize brand
+        @brand = brand
+    end
+
+    def properties brand = @brand, color, size, material, condition
+        @brand = brand
+        @color = color
+        @size = size
+        @material = material
+        @condition = condition
+    end
+
+    def cobble 
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+    end
+
+end
+
+shoe = Shoe.new("Adidas")
+shoe.properties("red", "42", "leather", "used")
+puts shoe.brand
+puts shoe.color
+puts shoe.size
+puts shoe.material
+puts shoe.condition
+shoe.cobble
+puts shoe.condition
